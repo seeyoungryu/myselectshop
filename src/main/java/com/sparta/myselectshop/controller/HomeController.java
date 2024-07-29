f2b2d456cf2417ebad9c85310ea6d3ab5dc4bc3c
seeyoungryu <seayeux@gmail.com>
2024-07-29 14:54:34 +0900

    update - 상품 검색 api 구현

--- src/main/java/com/sparta/myselectshop/controller/HomeController.java
+++ src/main/java/com/sparta/myselectshop/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.sparta.myselectshop.controller;public class HomeController {
+package com.sparta.myselectshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")            //슬래쉬로 요청 들어가면 인덱스.html 파일 반환함
+    public String home() {
+        return "index";
+    }
 }
+
